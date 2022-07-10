51f37573b8afa7621fdfc7c62e2c1521936fd41a
abraraboelmaaty <abraraboelmaaty24@gmail.com>
2022-07-10 19:10:09 +0200

    Add instructor controller and Repo

--- CoursatOnline/CoursatOnline/Models/Instructor.cs
+++ CoursatOnline/CoursatOnline/Models/Instructor.cs
@@ -1,9 +1,13 @@
-﻿namespace CoursatOnline.Models
+﻿using System.Text.Json.Serialization;
+
+namespace CoursatOnline.Models
 {
     public class Instructor : User
     {
         //List of Courses that can be added by instructor 
+        [JsonIgnore]
         public virtual List<Course>? _Courses { get; set; }
+        [JsonIgnore]
         public virtual List<Chapter>? _Chapters { get; set; }
     }
 }
